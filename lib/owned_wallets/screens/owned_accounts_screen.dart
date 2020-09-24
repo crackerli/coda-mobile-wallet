37b996b87939daae0b76c2a2d0870ed54a737b8e
crackerli <crackerli@163.com>
2020-09-24 23:21:14 +0800

    Update owned account page

--- lib/owned_wallets/screens/owned_accounts_screen.dart
+++ lib/owned_wallets/screens/owned_accounts_screen.dart
@@ -22,12 +22,7 @@ class _OwnedAccountsScreenState extends State<OwnedAccountsScreen> {
     super.initState();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  AppBar _buildAppBar() {
+  Widget _buildAppBar() {
     return AppBar(title: Text('My Accounts'));
   }
 
@@ -35,12 +30,12 @@ class _OwnedAccountsScreenState extends State<OwnedAccountsScreen> {
   Widget build(BuildContext context) {
     return BlocBuilder<OwnedAccountsBloc, OwnedAccountsStates>(
         builder: (BuildContext context, OwnedAccountsStates state) {
-          return _getStateWidget(context, state);
+          return _buildAccountsWidget(context, state);
         }
     );
   }
 
-  Widget _getStateWidget(BuildContext context, OwnedAccountsStates state) {
+  Widget _buildAccountsWidget(BuildContext context, OwnedAccountsStates state) {
     if(state is Loading) {
       return Scaffold(appBar: _buildAppBar(), body: LinearProgressIndicator());
     } else if(state is LoadDataFail) {
@@ -50,34 +45,66 @@ class _OwnedAccountsScreenState extends State<OwnedAccountsScreen> {
       return Scaffold(appBar: _buildAppBar(), body: _buildAccountListWidget(data));
     }
   }
+  
+  _accountsTapCallback(String publicKey) {
+    Map<String, dynamic> variables = Map<String, dynamic>();
+    variables['publicKey'] = publicKey;
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) {
+          return BlocProvider<AccountTxnsBloc>(
+              create: (BuildContext context) {
+                return AccountTxnsBloc(AccountStates.Loading())
+                  ..add(FetchAccountTxnsData(ACCOUNT_TXNS_QUERY, variables: variables));
+              },
+              child: AccountTxnsScreen(publicKey)
+          );
+        }));
+  }
+
+  Widget _buildAccountItem(BuildContext context, dynamic itemData) {
+    String publicKey = itemData['publicKey'];
+    return Container(
+      padding: EdgeInsets.only(left: 14.0, right: 14.0, top: 6.0, bottom: 1.0),
+      child: Column(
+        children: [
+          Text(publicKey, softWrap: true, textAlign: TextAlign.left, overflow: TextOverflow.ellipsis, maxLines: 1),
+          Container(height: 10),
+          Row(
+            children: [
+              Text("Token: 19856"),
+              Container(width: 10, height: 1),
+              Image.asset("images/coda_logo.png", width: 16, height: 16)
+            ]
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+      )
+    );
+  }
 
   Widget _buildAccountListWidget(dynamic data) {
     List ownedAccounts = data as List;
     return ListView.separated(
       itemCount: ownedAccounts.length,
       itemBuilder: (context, index) {
-        return ListTile(
-            title: Text(ownedAccounts[index]['publicKey']),
-            onTap: () {
-              String publicKey = ownedAccounts[index]['publicKey'];
-              Map<String, dynamic> variables = Map<String, dynamic>();
-              variables['publicKey'] = publicKey;
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) {
-                    return BlocProvider<AccountTxnsBloc>(
-                      create: (BuildContext context) {
-                        return AccountTxnsBloc(AccountStates.Loading())
-                          ..add(FetchAccountTxnsData(ACCOUNT_TXNS_QUERY, variables: variables));
-                      },
-                      child: AccountTxnsScreen(publicKey)
-                    );
-                  }));
-            }
-        );
+        String publicKey = ownedAccounts[index]['publicKey'];
+
+        // return ListTile(
+        //     title: Text(publicKey),
+        //     onTap: () { _accountsTapCallback(publicKey); }
+        // );
+        return _buildAccountItem(context, ownedAccounts[index]);
       },
       separatorBuilder: (context, index) {
         return Divider();
       },
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
