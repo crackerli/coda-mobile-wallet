4ccee7a60217c18a86fbc538dc61c222c8c53735
crackerli <crackerli@163.com>
2020-10-15 16:35:14 +0800

    screen adaptation of send token

--- lib/send_token/screens/send_token_screen.dart
+++ lib/send_token/screens/send_token_screen.dart
@@ -118,45 +118,34 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.only(left: 6.w),
-            child: Text('Receiver address',
-              style: TextStyle(fontSize: 33.sp, color: Color.fromARGB(0xff, 65, 69, 72))),
-          ),
+          _buildReceiverLabel(),
           Container(height: 34.h),
           _buildReceiverTextField(),
           Container(height: 32.h),
-          Padding(
-            padding: EdgeInsets.only(left: 6, right: 6),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Text('Balance', style: TextStyle(fontSize: 34.sp, color: Color.fromARGB(0xff, 90, 94, 97)),),
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    '${formatTokenNumber(widget.balance)} Mina',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(color: Color.fromARGB(0xff, 151, 154, 159),
-                    fontSize: 34.sp))
-                )
-              ]
-            ),
-          ),
+          _buildSendContentLabel(),
           Container(height: 10.h),
           _buildSendContentTextField(),
+          Container(height: 10.h),
           _buildFeeCostTextField(),
+          Container(height: 10.h),
           _buildLockStatus(),
-          Container(height: 40),
-          BlocBuilder<SendTokenBloc, SendTokenStates>(
-            builder:(BuildContext context, SendTokenStates state) {
-              return  _buildSendAction(context, state);
-            }
+          Expanded(flex: 1,
+            child: BlocBuilder<SendTokenBloc, SendTokenStates>(
+              builder:(BuildContext context, SendTokenStates state) {
+                return  _buildSendAction(context, state);
+              }
+            )
           )
         ]
       )
+    );
+  }
+
+  Widget _buildReceiverLabel() {
+    return Padding(
+      padding: EdgeInsets.only(left: 13.w),
+      child: Text('Receiver Address',
+        style: TextStyle(fontSize: 33.sp, color: Color.fromARGB(0xff, 65, 69, 72)))
     );
   }
 
@@ -164,9 +153,8 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
     return Card(
       elevation: 2,
       color: Colors.white,
-      child: Container(
-        height: 160.h,
-        padding: EdgeInsets.only(left: 10, right: 10, top: 6, bottom: 6),
+      child: Padding(
+        padding: EdgeInsets.only(left: 44.w, right: 44.w, top: 44.h, bottom: 44.h),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -176,8 +164,8 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
               flex: 10,
               child: Container(
                 constraints: BoxConstraints(
-                  maxHeight: 200.0,
-                  minHeight: 24.0,
+                  maxHeight: 300.h,
+                  minHeight: 37.h,
                 ),
                 child: TextField(
                   controller: _addressController,
@@ -194,7 +182,7 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
             ),
             Expanded(
               flex: 1,
-              child: Image.asset('images/contact_address.png', width: 24, height: 24)
+              child: Image.asset('images/contact_address.png', width: 60.w, height: 60.w)
             )
           ]
         )
@@ -202,24 +190,47 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
     );
   }
 
+  Widget _buildSendContentLabel() {
+    return Padding(
+      padding: EdgeInsets.only(left: 13.w, right: 13.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text('Balance', style: TextStyle(fontSize: 34.sp, color: Color.fromARGB(0xff, 90, 94, 97)),),
+          Expanded(
+            flex: 1,
+            child: Text(
+              '${formatTokenNumber(widget.balance)} Mina',
+              textAlign: TextAlign.right,
+              style: TextStyle(color: Color.fromARGB(0xff, 151, 154, 159),
+              fontSize: 34.sp))
+          )
+        ]
+      )
+    );
+  }
+
   Widget _buildSendContentTextField() {
     return Card(
-      child: Container(
-        height: 452.h,
-        padding: EdgeInsets.only(left: 10, right: 10, top: 6, bottom: 6),
+      child: Padding(
+        padding: EdgeInsets.only(left: 44.w, right: 44.w, top: 44.h, bottom: 44.h),
         child: Column(
           children: [
             TextField(
+              maxLines: 1,
               controller: _amountController,
               onChanged: (text) {
                 _sendTokenBloc.amount = text;
                 _sendTokenBloc.add(ValidateInput());
               },
               keyboardType: TextInputType.numberWithOptions(decimal: true),
-              style: TextStyle(fontSize: 48),
+              style: TextStyle(fontSize: 120.sp),
               decoration: InputDecoration.collapsed(hintText: '0')
             ),
-            Container(height: 1.5, color: Color(0xffeeeeee)),
+            Container(height: 6.h, color: Color(0xffeeeeee)),
+            Container(height: 24.h,),
             TextField(
               controller: _memoController,
               onChanged: (text) {
@@ -239,14 +250,14 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
 
   Widget _buildFeeCostTextField() {
     return Card(
-      child: Container(
-        padding: EdgeInsets.only(left: 10, right: 10, top: 6, bottom: 6),
+      child: Padding(
+        padding: EdgeInsets.only(left: 44.w, right: 44.w, top: 44.h, bottom: 44.h),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
-            Text('Fee'),
+            Text('Fee', style: TextStyle(fontSize: 41.sp, color: Color.fromARGB(0xff, 70, 70, 70))),
             Expanded(
               flex: 10,
               child: TextField(
@@ -262,8 +273,8 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
                 decoration: InputDecoration.collapsed(hintText: '${_sendTokenBloc.fee}'),
               )
             ),
-            Container(width: 10),
-            Text('Mina', style: TextStyle(color: Colors.blueAccent, fontWeight: FontWeight.bold))
+            Container(width: 20.w),
+            Text('Mina', style: TextStyle(color: Color.fromARGB(0xff, 152, 152, 152)))
           ]
         )
       )
@@ -272,15 +283,15 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
 
   Widget _buildLockStatus() {
     return Card(
-      child: Container(
-        padding: EdgeInsets.only(left: 10, right: 10, top: 6, bottom: 6),
+      child: Padding(
+        padding: EdgeInsets.only(left: 44.w, right: 44.w, top: 44.h, bottom: 44.h),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
-            Text('LockStatus'),
-            Image.asset('images/unlocked_green.png', width: 24, height: 24)
+            Text('LockStatus', style: TextStyle(fontSize: 41.sp, color: Color.fromARGB(0xff, 70, 70, 70))),
+            Image.asset('images/unlocked_green.png', width: 60.w, height: 60.w)
           ]
         )
       )
@@ -297,7 +308,7 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
       sendButtonColor = Colors.blueAccent;
       sendAction = SizedBox(child: CircularProgressIndicator(
         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-      ), height: 26.0, width: 26.0);
+      ), height: 68.h, width: 68.w);
     } else if(state is SendPaymentSuccess) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
         final snackBar = SnackBar(content: Text('Mina sent'));
@@ -305,27 +316,38 @@ class _SendTokenScreenState extends State<SendTokenScreen> {
       });
       sendTextColor = Colors.white;
       sendButtonColor = Colors.blueAccent;
-      sendAction = Text("Send", style: TextStyle(color: sendTextColor));
+      sendAction = Text("Send", style: TextStyle(color: sendTextColor, fontSize: 44.sp));
     } else if(state is InputInvalidated) {
       sendTextColor = Colors.white;
       sendButtonColor = Colors.grey;
-      sendAction = Text("Send", style: TextStyle(color: sendTextColor));
+      sendAction = Text("Send", style: TextStyle(color: sendTextColor, fontSize: 44.sp));
     } else {
       sendTextColor = Colors.white;
       sendButtonColor = Colors.blueAccent;
-      sendAction = Text("Send", style: TextStyle(color: sendTextColor));
+      sendAction = Text("Send", style: TextStyle(color: sendTextColor, fontSize: 44.sp));
     }
 
     return Container(
-      height: 50,
-      child: SizedBox.expand(
-        child: RaisedButton(
-          padding: EdgeInsets.only(top: 12, bottom: 12),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(4.0))),
-          onPressed: _sendPayment,
-          color: sendButtonColor,
-          child: sendAction
-        )
+      padding: EdgeInsets.only(left: 32.w, right: 32.w, bottom: 64.h),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Container(
+            height: 160.h,
+            child: SizedBox.expand(
+              child: RaisedButton(
+                padding: EdgeInsets.only(top: 2.h, bottom: 2.h),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                onPressed: _sendPayment,
+                color: sendButtonColor,
+                child: sendAction
+              )
+            )
+          )
+        ]
       )
     );
   }
