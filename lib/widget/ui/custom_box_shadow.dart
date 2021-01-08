63aae3c4d4efa45ac6fae4180a9c6604b4ff88e1
crackerli <crackerli@163.com>
2021-01-08 15:33:44 +0800

    add customized boxshadow

---
+++ lib/widget/ui/custom_box_shadow.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomBoxShadow extends BoxShadow {
+  final BlurStyle blurStyle;
+
+  const CustomBoxShadow({
+    Color color = const Color(0xFF000000),
+    Offset offset = Offset.zero,
+    double blurRadius = 0.0,
+    this.blurStyle = BlurStyle.normal,
+  }) : super(color: color, offset: offset, blurRadius: blurRadius);
+
+  @override
+  Paint toPaint() {
+    final Paint result = Paint()
+      ..color = color
+      ..maskFilter = MaskFilter.blur(this.blurStyle, blurSigma);
+    assert(() {
+      if (debugDisableShadows)
+        result.maskFilter = null;
+      return true;
+    }());
+    return result;
+  }
+}
