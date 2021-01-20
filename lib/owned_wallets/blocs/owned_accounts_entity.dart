6f4541c608c815d625147b8a002494316ba9d661
crackerli <crackerli@163.com>
2021-01-20 12:45:17 +0800

    remove useless old resources

--- lib/owned_wallets/blocs/owned_accounts_entity.dart
+++
@@ -1,8 +0,0 @@
-class Account {
-  Account({this.publicKey, this.locked, this.balance});
-
-  final String publicKey;
-  final String balance;
-  final bool locked;
-  String accountName;
-}
--- lib/util/navigations.dart
+++ lib/util/navigations.dart
@@ -1,32 +1,24 @@
-import 'package:coda_wallet/account_txns/blocs/account_txns_bloc.dart';
-import 'package:coda_wallet/account_txns/blocs/account_txns_entity.dart';
-import 'package:coda_wallet/account_txns/blocs/account_txns_states.dart';
-import 'package:coda_wallet/account_txns/screens/account_txns_screen.dart';
-import 'package:coda_wallet/owned_wallets/blocs/owned_accounts_entity.dart';
 import 'package:coda_wallet/qr_address/qr_address_screen.dart';
 import 'package:coda_wallet/qr_scan/qr_scanner.dart';
 import 'package:coda_wallet/route/routes.dart';
-import 'package:coda_wallet/send/blocs/send_token_bloc.dart';
-import 'package:coda_wallet/send/blocs/send_token_states.dart';
-import 'package:coda_wallet/send/screens/send_token_screen.dart';
+//import 'package:coda_wallet/send/screens/send_token_screen.dart';
 import 'package:coda_wallet/setting/network_setting_screen.dart';
-import 'package:coda_wallet/txn_detail/screens/txn_info_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-toAccountTxnsScreen(BuildContext context, Account account) {
-
-  Navigator.push(context,
-    MaterialPageRoute(builder: (context) {
-      return BlocProvider<AccountTxnsBloc>(
-        create: (BuildContext context) {
-          return AccountTxnsBloc(RefreshAccountTxnsLoading(null), account.publicKey);
-        },
-        child: AccountTxnsScreen(account)
-      );
-    }
-  ));
-}
+//toAccountTxnsScreen(BuildContext context, Account account) {
+//
+//   Navigator.push(context,
+//     MaterialPageRoute(builder: (context) {
+//       return BlocProvider<AccountTxnsBloc>(
+//         create: (BuildContext context) {
+//           return AccountTxnsBloc(RefreshAccountTxnsLoading(null), account.publicKey);
+//         },
+//         child: AccountTxnsScreen(account)
+//       );
+//     }
+//   ));
+// }
 
 toQrAddressScreen(BuildContext context, String publicKey) {
   Navigator.push(context,
@@ -34,18 +26,18 @@ toQrAddressScreen(BuildContext context, String publicKey) {
   );
 }
 
-toSendTokenScreen(BuildContext context, String publicKey, String balance, bool locked, bool isDelegation) {
-  Navigator.push(context,
-    MaterialPageRoute(builder: (context) {
-      return BlocProvider<SendTokenBloc>(
-        create: (BuildContext context) {
-          return SendTokenBloc(InputInvalidated());
-        },
-        child: SendTokenScreen(publicKey, balance, locked, isDelegation)
-      );
-    }
-  ));
-}
+// toSendTokenScreen(BuildContext context, String publicKey, String balance, bool locked, bool isDelegation) {
+//   Navigator.push(context,
+//     MaterialPageRoute(builder: (context) {
+//       return BlocProvider<SendTokenBloc>(
+//         create: (BuildContext context) {
+//           return SendTokenBloc(InputInvalidated());
+//         },
+//         child: SendTokenScreen(publicKey, balance, locked, isDelegation)
+//       );
+//     }
+//   ));
+// }
 
 dynamic toQrScanScreen(BuildContext context) async {
   final result = Navigator.pushNamed(context, QrScanRoute);
@@ -59,8 +51,8 @@ dynamic toSettingScreen(BuildContext context) async {
   return result;
 }
 
-toTxnDetailScreen(BuildContext context, MergedUserCommand mergedUserCommand, String publicKey) {
-  Navigator.push(context,
-    MaterialPageRoute(builder: (context) => TxnDetailScreen(publicKey: publicKey, mergedUserCommand: mergedUserCommand))
-  );
-}
+// toTxnDetailScreen(BuildContext context, MergedUserCommand mergedUserCommand, String publicKey) {
+//   Navigator.push(context,
+//     MaterialPageRoute(builder: (context) => TxnDetailScreen(publicKey: publicKey, mergedUserCommand: mergedUserCommand))
+//   );
+// }
