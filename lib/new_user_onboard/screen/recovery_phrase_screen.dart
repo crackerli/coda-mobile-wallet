60c3ee9c24b21553819380cd668a4c21f101854e
crackerli <crackerli@163.com>
2021-01-26 16:08:07 +0800

    update recovery phrase screen

--- lib/new_user_onboard/screen/recovery_phrase_screen.dart
+++ lib/new_user_onboard/screen/recovery_phrase_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:ui';
 import 'package:coda_wallet/route/routes.dart';
 import 'package:coda_wallet/widget/app_bar/app_bar.dart';
+import 'package:coda_wallet/widget/ui/custom_box_shadow.dart';
 import 'package:ffi_mina_signer/sdk/mina_signer_sdk.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RecoveryPhraseScreen extends StatefulWidget {
@@ -34,7 +36,10 @@ class _RecoveryPhraseScreenState extends State<RecoveryPhraseScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: buildNoTitleAppBar(context, actions: false),
-      body: _buildRecoveryPhraseBody()
+      body: Padding(
+        padding: EdgeInsets.only(left: 28.w, right: 28.w),
+        child: _buildRecoveryPhraseBody()
+      )
     );
   }
 
@@ -43,28 +48,58 @@ class _RecoveryPhraseScreenState extends State<RecoveryPhraseScreen> {
       alignment: Alignment.center,
       children: [
         Column(children: [
-          Container(height: 33.h),
-          Text('Recovery Phrase', style: TextStyle(fontSize: 25.sp, color: Colors.black, fontWeight: FontWeight.w400)),
-          Container(height: 37.h),
+          Image.asset('images/password_unlock_logo.png', width: 100.h, height: 100.h,),
+          Container(height: 21.h),
+          Text('My Recovery Phrase', style: TextStyle(fontSize: 28.sp, color: Color(0xff2d2d2d))),
+          Container(height: 29.h),
           _buildRecoveryPhraseTable(),
-          Container(height: 33.h),
-          Text('Copy to clipboard', style: TextStyle(fontSize: 18.sp, color: Colors.black, fontWeight: FontWeight.w700)),
-          Container(height: 33.h),
+          Container(height: 27.h),
           Padding(
-            padding: EdgeInsets.only(left: 36.w, right: 36.w),
-            child: Text('Write these 12 words down, or copy them to a password manager.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16.sp, color: Colors.black, fontWeight: FontWeight.w400)),
+            padding: EdgeInsets.only(left: 12.w, right: 12.w),
+            child: Text('These 12 words are the keys to your wallet.\n\nBack them up manually, or store them in your password manager.',
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 14.sp, color: Colors.black)),
+          ),
+          Container(height: 32.h),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.w),
+              color: Color(0xffeeeeee),
+              border: Border.all(color: Color(0xffeeeeee), width: 1.w)
             ),
+            padding: EdgeInsets.only(top: 10.h, bottom: 10.h, left: 42.w, right: 42.w),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Builder(builder: (context) =>
+                  InkWell(
+                    child: Image.asset('images/copy_gray.png', width: 22.w, height: 27.h),
+                    onTap: () {
+                      Clipboard.setData(ClipboardData(text: _mnemonic));
+                      Scaffold.of(context).showSnackBar(SnackBar(content: Text('Recovery phrase copied into clipboard!!')));
+                    },
+                  )
+                ),
+                Container(width: 6.w,),
+                Text('COPY TO CLIPBOARD', textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w500, color: Color(0xff2d2d2d))),
+              ],
+            )
+          )
         ]),
         Positioned(
-          bottom: 84.h,
-          child: RaisedButton(
-            padding: EdgeInsets.only(top: 11.h, bottom: 11.h, left: 100.w, right: 100.w),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.w))),
-            onPressed: () => Navigator.pushNamed(context, VerifyRecoveryPhraseRoute, arguments: _mnemonic),
-            color: Colors.blueAccent,
-            child: Text('Continue', style: TextStyle(fontSize: 17.sp, color: Colors.white, fontWeight: FontWeight.w600))
+          bottom: 67.h,
+          child: InkWell(
+            onTap: () => Navigator.of(context).pushNamed(VerifyRecoveryPhraseRoute, arguments: _mnemonic),
+            child: Container(
+              padding: EdgeInsets.only(top: 14.h, bottom: 14.h, left: 100.w, right: 100.w),
+              decoration: getMinaButtonDecoration(topColor: Color(0xffeeeeee)),
+              child: Text('CONTINUE',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w500, color: Color(0xff2d2d2d))),
+            )
           ),
         )
       ],
@@ -75,16 +110,28 @@ class _RecoveryPhraseScreenState extends State<RecoveryPhraseScreen> {
     List<String> words = _mnemonic.split(' ');
 
     return Container(
-      margin: EdgeInsets.only(left: 48.w, right: 48.w),
-      padding: EdgeInsets.only(left: 10.w, right: 10.w, top: 10.h, bottom: 10.h),
-      color: Color(0xffededed),
+      decoration: BoxDecoration(
+        color: Color(0xfffafafa),
+        borderRadius: BorderRadius.all(Radius.circular(0.w)),
+        border: Border.all(color: Color(0xffd9d9d9), width: 1.w)
+      ),
+      padding: EdgeInsets.only(left: 25.w, right: 25.w, top: 20.h, bottom: 20.h),
       child: Wrap(
         spacing: 10.w,
         runSpacing: 15.h,
           children: List.generate(words.length, (index) {
-            return Text(
-              '${words[index]}',
-              style: TextStyle(color: Colors.blue, fontSize: 18.sp, fontWeight: FontWeight.w400),
+            return RichText(
+              textAlign: TextAlign.left,
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '${index + 1} ',
+                    style: TextStyle(fontSize: 14.sp, color: Color(0xff9e9e9e))),
+                  TextSpan(
+                    text: '${words[index]}',
+                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.normal, fontSize: 14.sp)),
+                ]
+              )
             );
           }
         )
