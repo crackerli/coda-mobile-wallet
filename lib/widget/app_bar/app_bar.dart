de2f6273fc3ddcafdeafdb732a1d0530201b9799
crackerli <crackerli@163.com>
2021-03-10 21:04:33 +0800

    fix UI walkthrough issue

--- lib/widget/app_bar/app_bar.dart
+++ lib/widget/app_bar/app_bar.dart
@@ -10,7 +10,7 @@ buildNoTitleAppBar(BuildContext context, {bool leading = true, bool actions = tr
         InkWell(
           child: Padding(
             padding: EdgeInsets.only(right: 14.w),
-            child: actions ? Image.asset('images/close.png', width: 28.w, height: 28.h,) : Container(),
+            child: actions ? Image.asset('images/close.png', width: 18.w, height: 18.w,) : Container(),
           ),
           onTap: () => Navigator.of(context).pop(),
         )
