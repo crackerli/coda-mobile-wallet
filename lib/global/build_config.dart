f3ed64bd1951329f8cf73bd66aed963f0924016c
crackerli <crackerli@163.com>
2021-03-10 12:32:39 +0800

    update comments of build config

--- lib/global/build_config.dart
+++ lib/global/build_config.dart
@@ -1,8 +1,8 @@
 import 'package:coda_wallet/constant/constants.dart';
 
 // If debugConfig is true, then we will open the http proxy in coda_service.dart, else we will close it.
-// Be sure this global var will be changed by build script.
 bool debugConfig = false;
 
-// This will be rewrite while using script to start the building.
+// If using 'TEST_NET_ID', then the generated signature can be only used in Mina testnet, such as TestWorld
+// If using 'MAIN_NET_ID', then the generated signature can be only used in Mina main net.
 int networkIdConfig = TEST_NET_ID;
