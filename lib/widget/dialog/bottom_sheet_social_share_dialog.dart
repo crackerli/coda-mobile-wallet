f783594e4e13c41c9088adf53e65f11d1ebde82d
1ming <pyedog@sina.cn>
2023-03-02 17:22:59 +0800

    1. Reformat code

--- lib/widget/dialog/bottom_sheet_social_share_dialog.dart
+++ lib/widget/dialog/bottom_sheet_social_share_dialog.dart
@@ -11,10 +11,12 @@ import 'package:image_picker/image_picker.dart';
 List<String> _shareNames = [
   'Facebook',
   'Twitter',
+  // todo
   // 'Wechat',
   'Instagram',
   'Telegram',
   'Whatsapp',
+  // todo
   // 'SMS',
 ];
 List<String> _shareIcons = [
@@ -43,7 +45,8 @@ List<Function> _shareMethods = [
   (String address, String? path) async {
     //facebook appId is mandatory for andorid or else share won't work
     if(Platform.isAndroid) {
-      _appinioSocialShare.shareToFacebook('My mina address: $address', path!);
+      String result = await _appinioSocialShare.shareToFacebook('My mina address: $address', path!);
+      print(result);
     } else {
       XFile? file = await ImagePicker().pickImage(source: ImageSource.gallery);
       if(null != file) {
@@ -54,6 +57,7 @@ List<Function> _shareMethods = [
   (String address, String? path) async {
     _appinioSocialShare.shareToTwitter('My mina address: $address', filePath: path!);
   },
+  // todo
   // (String address, String? path) async {
   //   _appinioSocialShare.shareToWeChat('My mina address: $address',
   //       filePath: path!);
@@ -78,15 +82,13 @@ List<Function> _shareMethods = [
   (String address, String? path) async {
     _appinioSocialShare.shareToWhatsapp('My mina address: $address', filePath: path!);
   },
-  (String address, String? path) async {
-    _appinioSocialShare.shareToSMS('My mina address: $address', filePath: path!);
-  },
+  // todo
   // (String address, String? path) async {
   //   _appinioSocialShare.shareToSMS('My mina address: $address', filePath: path!);
   // },
 ];
 
-showSocialShareSheet(BuildContext context, String? address, String? snapShotPath, Map? installedApp) {
+showSocialShareSheet(BuildContext context, String? address, String? snapshotPath, Map? installedApp) {
   BottomSheetDialog.bottomMaterialDialog(
     title: 'Select an application to share',
     titleStyle: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500),
@@ -101,7 +103,7 @@ showSocialShareSheet(BuildContext context, String? address, String? snapShotPath
             height: 120.h,
             child: ListView(
               shrinkWrap: true,
-              children: _budildList(context, address, snapShotPath, installedApp),
+              children: _buildList(context, address, snapshotPath, installedApp),
               scrollDirection: Axis.horizontal,
             )
           )
@@ -110,7 +112,7 @@ showSocialShareSheet(BuildContext context, String? address, String? snapShotPath
   );
 }
 
-String _getIconPath(Map? installedApp, int index) {
+String _getAppIconFromAssets(Map? installedApp, int index) {
   if (installedApp != null) {
     return installedApp[_shareNames[index].toLowerCase()] ? _shareIcons[index] : _shareDisableIcons[index];
   }
@@ -118,19 +120,18 @@ String _getIconPath(Map? installedApp, int index) {
   return _shareDisableIcons[index];
 }
 
-List<Widget> _budildList(BuildContext context, String? address, String? snapShotPath, Map? installedApp) {
+List<Widget> _buildList(BuildContext context, String? address, String? snapshotPath, Map? installedApp) {
   List<Widget> result = <Widget>[];
   for (int i = 0; i < _shareNames.length; i++) {
-    result.add(_item(context, address, snapShotPath, installedApp, i));
+    result.add(_item(context, address, snapshotPath, installedApp, i));
   }
   return result;
 }
 
-Widget _item(BuildContext context, String? address, String? snapShotPath, Map? installedApp, int index) {
-  return Card(
-    child: InkWell(
-      onTap: () => _onPressShareItem(
-        context, address, snapShotPath, installedApp, index),
+Widget _item(BuildContext context, String? address, String? snapshotPath, Map? installedApp, int index) {
+  return InkWell(
+    onTap: () => _onPressShareItem(context, address, snapshotPath, installedApp, index),
+    child: Card(
       child: Container(
         width: 80.w,
         height: 60.w,
@@ -138,8 +139,8 @@ Widget _item(BuildContext context, String? address, String? snapShotPath, Map? i
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Image.asset(_getIconPath(installedApp, index), height: 45.w, width: 45.w),
-              const SizedBox(height: 5),
+              Image.asset(_getAppIconFromAssets(installedApp, index), height: 45.w, width: 45.w),
+              Container(height: 5),
               Text(_shareNames[index], style: TextStyle(fontSize: 14.sp, color: Colors.black)),
             ],
           ),
@@ -149,10 +150,10 @@ Widget _item(BuildContext context, String? address, String? snapShotPath, Map? i
   );
 }
 
-_onPressShareItem(BuildContext context, String? address, String? snapShotPath,
+_onPressShareItem(BuildContext context, String? address, String? snapshotPath,
     Map? installedApp, int index) {
   if (null != installedApp && (installedApp[_shareNames[index].toLowerCase()])) {
-    _shareMethods[index](address, snapShotPath);
+    _shareMethods[index](address, snapshotPath);
     Navigator.of(context).pop();
   } else {
     Fluttertoast.showToast(
